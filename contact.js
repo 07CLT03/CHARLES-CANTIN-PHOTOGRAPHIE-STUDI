29264f4cc625a1b654e8408237293acf3fa04be0
Charlotte Lft <charlotte.lft@hotmail.be>
2023-05-27 18:45:58 +0200

    ajout animation page contact

---
+++ contact.js
@@ -0,0 +1,32 @@
+
+
+
+// ANIMATION FOOTER   - SLIDE DOWN TO UP 
+
+gsap.registerPlugin(ScrollTrigger); 
+
+gsap.from(".footer-card-left",{
+  y:100,
+  opacity:0,  
+  duration:3, 
+  scale:0.5,
+  scrollTrigger: ".copyright"
+})
+
+gsap.from(".footer-card-center",{
+  y:100,
+  opacity:0,  
+  duration:3, 
+  scale:0.5,
+  delay:1,
+  scrollTrigger: ".copyright"
+})
+
+gsap.from(".footer-card-right",{
+  y:100,
+  opacity:0,  
+  duration:2, 
+  scale:0.5,
+  delay:2.6,
+  scrollTrigger: ".animation-trigger-slide-in"
+})
